8a1c1f3572b8c69cf0b925b38e59ead2580fd37f
philt <pierre.hilt@gmail.com>
2019-11-03 22:05:26 +0100

    feat(install): install NPM package

---
+++ app/install.js
@@ -0,0 +1,38 @@
+/**
+ * Take care of the creation of the temporary package to install the package
+ * that we want to have the size
+ */
+
+const fs = require('fs-extra');
+const path = require('path');
+const childProcess = require('child_process');
+const shortId = require('shortid');
+
+const _installPath = 'tmp/package-build';
+
+const Install = {
+  generateInstall(packageName) {
+    const id = shortId.generate().slice(0, 3);
+    return path.join(_installPath, `${packageName}-${id}`);
+  },
+  installPackage(packageName, installPath, version) {
+    fs.mkdirpSync(installPath);
+    fs.writeFileSync(path.join(installPath, 'package.json'), JSON.stringify({ dependencies: {} }));
+    if (version) {
+      packageName += '@' + version;
+    }
+    childProcess.execSync(
+      `npm install ${packageName} --no-package-lock --progress false --loglevel error`,
+      {
+        cwd: installPath,
+        maxBuffer: 1024 * 500
+      }
+    );
+    return installPath;
+  },
+  cleanUp() {
+    return fs.remove(_installPath);
+  }
+};
+
+module.exports = Install;
---
+++ app/install.test.js
@@ -0,0 +1,31 @@
+const install = require('./install');
+const fs = require('fs-extra');
+const path = require('path');
+
+describe('install util', () => {
+  afterAll(done => {
+    fs.remove('tmp').then(_ => done());
+  });
+
+  test('should install a package without a version', () => {
+    const installPath = install.generateInstall('react');
+    install.installPackage('react', installPath);
+    expect(installPath).toEqual(expect.stringContaining('react'));
+    expect(fs.existsSync(path.join(installPath, 'node_modules/react'))).toBeTruthy();
+    const packageJson = require(path.resolve(
+      path.join(installPath, 'node_modules/react/package.json')
+    ));
+    expect(packageJson.version).toBeDefined();
+  });
+
+  test('should install a package with a version', () => {
+    const installPath = install.generateInstall('react');
+    install.installPackage('react', installPath, '15.0.0');
+    expect(installPath).toEqual(expect.stringContaining('react'));
+    expect(fs.existsSync(path.join(installPath, 'node_modules/react'))).toBeTruthy();
+    const packageJson = require(path.resolve(
+      path.join(installPath, 'node_modules/react/package.json')
+    ));
+    expect(packageJson.version).toEqual('15.0.0');
+  });
+});
