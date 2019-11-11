42d92f5488ab029633f07c99b12071e1bef9c2f5
philt <pierre.hilt@gmail.com>
2019-11-11 23:25:36 +0100

    feat(server): put in place express

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const stats = require('./app/stats');
+const validate = require('validate-npm-package-name');
+
+app.get('/', (req, res) =>
+  res.send('Application root.\nEntry point: stats?package={package name}')
+);
+
+app.get('/api/stats', async function(req, res) {
+  const packageName = req.query.package;
+  const validateResults = validate(packageName);
+  if (validateResults.errors) {
+    res.status(500).send({
+      error: 'Package name not valid'
+    });
+  } else {
+    try {
+      const packageStats = await stats(packageName);
+      res.send(packageStats);
+    } catch (err) {
+      res.status(500).send({
+        error: err.toString()
+      });
+    }
+  }
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
