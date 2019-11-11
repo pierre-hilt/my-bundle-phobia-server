ae200f241298e5633e0e933d2e893ab22db29304
philt <pierre.hilt@gmail.com>
2019-11-11 22:54:08 +0100

    feat(stats): manage missing module

--- app/stats.js
+++ app/stats.js
@@ -6,13 +6,30 @@ const zlib = require('zlib');
 const fs = require('fs-extra');
 const path = require('path');
 
-function getMinifiedSize(stats) {
-  return stats.toJson().assets.find(asset => asset.name === 'main.bundle.js').size;
+function getMinifiedSize(jsonStats) {
+  return jsonStats.assets.find(asset => asset.name === 'main.bundle.js').size;
 }
 
 function getGzipSize(installPath) {
   const bundleContents = fs.readFileSync(path.join(installPath, 'main.bundle.js'));
   return zlib.gzipSync(bundleContents, {}).length;
+}
+
+function getModuleNotFoundError(errors) {
+  const moduleNotFoundErrors = errors.filter(
+    error => error.indexOf('Module not found: Error') !== -1
+  );
+
+  if (moduleNotFoundErrors.length === 0) {
+    return [];
+  }
+
+  const moduleNotFoundsRegex = /Can't resolve '(.+)' in/;
+
+  return moduleNotFoundErrors.map(err => {
+    const matches = err.toString().match(moduleNotFoundsRegex);
+    return matches[1];
+  });
 }
 
 async function stats(packageName) {
@@ -25,8 +42,22 @@ async function stats(packageName) {
     install.installPackage(versionedName, installPath);
     const { stats } = await build.compile(packageName, installPath);
 
+    let jsonStats = stats.toJson();
+
+    if (jsonStats.errors) {
+      const externals = getModuleNotFoundError(jsonStats.errors);
+      if (externals) {
+        // In this case there is a needed module not installed (peer dependency)
+        // We need to mark it as externals for webpack
+        const { stats } = await build.compile(packageName, installPath, externals);
+        jsonStats = stats.toJson();
+      } else {
+        throw new Error('Compilation error');
+      }
+    }
+
     buildStats[versionToInstall] = {
-      size: getMinifiedSize(stats),
+      size: getMinifiedSize(jsonStats),
       gzip: getGzipSize(installPath)
     };
   }
